ecd7c32f19e03ac804585c86ed08b330cd3f3b7f
Quentin Buzet <quentin.buzet@telecom-paris.fr>
2023-03-28 00:57:25 +0200

    Premier essai d'une boucle sur T (non fonctionnel)

--- main.c
+++ main.c
@@ -13,11 +13,14 @@
 #define NB_LIGNES 5
 #define NB_COLONNES 5
 #define SPIN_DEF 0 //0 = GRILLE ALEATOIRE, 1 = GRILLE UP, -1 = GRILLE DOWN
-#define NB_ITERATIONS 100
+#define NB_ITERATIONS 1000
 
 #define J 1.0
 #define kB 1.0
-#define T 10.0
+
+#define T_MIN 1.0
+#define T_MAX 5.0
+#define NB_TEMPERATURES 100
 
 #define FILENAME "./out.txt"
 
@@ -32,99 +35,135 @@ int main(int argc, char* argv[]){
         srand(SEED);
     }
 
-    //Initialisation des tableaux contenant les valeurs successives de l'énergie et l'aimantation
-    int tabE[NB_ITERATIONS+1];
-    int tabM[NB_ITERATIONS+1];
+    //Initialisation des tableaux contenant les valeurs successives de l'énergie moyenne et l'aimantation moyenne à T fixé
+    double tabEMoy[NB_TEMPERATURES];
+    double tabMMoy[NB_TEMPERATURES];
 
-    //Initialisation de la grille
-    int grille[NB_LIGNES][NB_COLONNES];
+    for(int t=0; t < NB_TEMPERATURES; t++){
+        double T = T_MIN + t * (T_MAX - T_MIN) / NB_TEMPERATURES;
 
-    for(int i = 0; i < NB_LIGNES; i++){
-        for(int j = 0; j < NB_COLONNES; j++){
-            if(SPIN_DEF == 0){
-                int spin = (rand() % 2 == 0) ? -1 : 1;
-                grille[i][j] = spin;
-            } else {
-                grille[i][j] = SPIN_DEF;
+        //Initialisation de la grille
+        int grille[NB_LIGNES][NB_COLONNES];
+
+        for(int i = 0; i < NB_LIGNES; i++){
+            for(int j = 0; j < NB_COLONNES; j++){
+                if(SPIN_DEF == 0){
+                    int spin = (rand() % 2 == 0) ? -1 : 1;
+                    grille[i][j] = spin;
+                } else {
+                    grille[i][j] = SPIN_DEF;
+                }
             }
         }
-    }
 
-    //Affichage de la grille
-    for(int i = 0; i < NB_LIGNES; i++){
-        for(int j = 0; j < NB_COLONNES; j++){
-            printf("%d ", grille[i][j]);
+        //Affichage de la grille
+        /*for(int i = 0; i < NB_LIGNES; i++){
+            for(int j = 0; j < NB_COLONNES; j++){
+                printf("%d ", grille[i][j]);
+            }
+            printf("\n");
+        }*/
+
+        //Initialisation des tableaux contenant les valeurs successives de l'énergie et l'aimantation
+        int tabE[NB_ITERATIONS+1];
+        int tabM[NB_ITERATIONS+1];
+
+        double EMoy = 0;
+        double MMoy = 0;
+
+        //Cases adjacentes
+        int d[4][2] = {{0,1},{0,-1},{1,0},{-1,0}};
+
+        //Initialisation de l'énergie
+        int E = 0;
+        for(int i = 0; i < NB_LIGNES; i++){
+            for(int j = 0; j < NB_COLONNES; j++){
+                
+                for(int k = 0; k < 4; k++){
+                    int iv = (i + d[k][0]) % NB_LIGNES;
+                    int jv = (j + d[k][1]) % NB_COLONNES;
+                    E -= grille[i][j] * grille[iv][jv];
+                }
+            }
         }
-        printf("\n");
-    }
 
-    //Cases adjacentes
-    int d[4][2] = {{0,1},{0,-1},{1,0},{-1,0}};
+        //Initialisation de l'aimantation
+        int M = 0;
+        for (int i = 0; i < NB_LIGNES; i++) {
+            for (int j = 0; j < NB_COLONNES; j++) {
+                M += grille[i][j];
+            }
+        }
 
-    //Initialisation de l'énergie
-    int E = 0;
-    for(int i = 0; i < NB_LIGNES; i++){
-        for(int j = 0; j < NB_COLONNES; j++){
-            
+        //On stocke les valeurs initiales
+        tabE[0] = E;
+        tabM[0] = M;
+
+        for(int n = 1; n <= NB_ITERATIONS; n++){
+
+            //Choix d'un spin à renverser
+            int spin_ligne = randint(0, NB_LIGNES-1); //Exclu ici
+            int spin_colonne = randint(0, NB_COLONNES-1); //Exclu ici
+            //printf("Spin à renverser : %d %d\n", spin_ligne, spin_colonne);
+
+            //Calcul de l'énergie de la nouvelle configuration
+            int nE = E;
             for(int k = 0; k < 4; k++){
-                int iv = (i + d[k][0]) % NB_LIGNES;
-                int jv = (j + d[k][1]) % NB_COLONNES;
-                E -= grille[i][j] * grille[iv][jv];
+                int iv = (spin_ligne + d[k][0]) % NB_LIGNES;
+                int jv = (spin_colonne + d[k][1]) % NB_COLONNES;
+                nE -= (-2*grille[spin_ligne][spin_colonne]) * grille[iv][jv]; //2* car il faut ajouter également l'énergie déjà présente
             }
-        }
-    }
 
-    //Initialisation de l'aimantation
-    int M = 0;
-    for (int i = 0; i < NB_LIGNES; i++) {
-        for (int j = 0; j < NB_COLONNES; j++) {
-            M += grille[i][j];
-        }
-    }
+            //Calcul de l'aimantation de la nouvelle configuration
+            int nM = M - 2*grille[spin_ligne][spin_colonne]; //il faut soustraire en plus l'état d'aimantation actuel de la case 
 
-    //On stocke les valeurs initiales
-    tabE[0] = E;
-    tabM[0] = M;
-
-    for(int n = 1; n <= NB_ITERATIONS; n++){
-
-        //Choix d'un spin à renverser
-        int spin_ligne = randint(0, NB_LIGNES-1); //Exclu ici
-        int spin_colonne = randint(0, NB_COLONNES-1); //Exclu ici
-        printf("Spin à renverser : %d %d\n", spin_ligne, spin_colonne);
-
-        //Calcul de l'énergie de la nouvelle configuration
-        int nE = E;
-        for(int k = 0; k < 4; k++){
-            int iv = (spin_ligne + d[k][0]) % NB_LIGNES;
-            int jv = (spin_colonne + d[k][1]) % NB_COLONNES;
-            nE -= (-2*grille[spin_ligne][spin_colonne]) * grille[iv][jv]; //2* car il faut ajouter également l'énergie déjà présente
-        }
-
-        //Calcul de l'aimantation de la nouvelle configuration
-        int nM = M - 2*grille[spin_ligne][spin_colonne]; //il faut soustraire en plus l'état d'aimantation actuel de la case 
-
-        int DE = nE - E;
-        if(DE < 0){
-            //Renversement du spin
-            grille[spin_ligne][spin_colonne] = -grille[spin_ligne][spin_colonne];
-            E = nE;
-            M = nM;
-        } else {
-            int x = rand01();
-            if(x < exp((double) (DE / (kB * T)))){
-                //On accepte la nouvelle configuration
+            int DE = nE - E;
+            if(DE < 0){
+                //Renversement du spin
                 grille[spin_ligne][spin_colonne] = -grille[spin_ligne][spin_colonne];
                 E = nE;
                 M = nM;
             } else {
-                //On rejette la nouvelle configuration (rien à faire)
+                int x = rand01();
+                if(x < exp((-1.0) * (double) (DE / (kB * T)))){
+                    //On accepte la nouvelle configuration
+                    grille[spin_ligne][spin_colonne] = -grille[spin_ligne][spin_colonne];
+                    E = nE;
+                    M = nM;
+                } else {
+                    //On rejette la nouvelle configuration (rien à faire)
+                }
             }
+
+            //On stocke les nouvelles valeurs de l'énergie et de l'aimantation
+            tabE[n] = E;
+            tabM[n] = M;
         }
 
-        //On stocke les nouvelles valeurs de l'énergie et de l'aimantation
-        tabE[n] = E;
-        tabM[n] = M;
+        //Calcul de l'énergie et de l'aimantation moyenne
+        for (int n = 0; n < NB_ITERATIONS + 1; n++) {
+            EMoy += (double) tabE[n];
+            MMoy += (double )tabM[n];
+        }
+        EMoy /= (double)(NB_ITERATIONS + 1);
+        MMoy /= (double)(NB_ITERATIONS + 1);
+
+        tabEMoy[t] = EMoy;
+        tabMMoy[t] = MMoy;
+
+        //Ecriture de l'echantillonnage de l'énergie et de l'aimantation moyenne dans le fichier de sortie
+        /*FILE* file = fopen(FILENAME, "w+");
+        if (!file) {
+            printf("Impossible d'ouvrir le fichier");
+        }
+
+        fprintf(file, "#n E M \n");
+        for (int n = 0; n <= NB_ITERATIONS; n++) {
+            fprintf(file, "%d %d %d \n", n, tabE[n], tabM[n]);
+        }
+
+        fclose(file);
+        */
     }
 
     //Ecriture des résultats dans le fichier de sortie
@@ -133,9 +172,10 @@ int main(int argc, char* argv[]){
         printf("Impossible d'ouvrir le fichier");
     }
 
-    fprintf(file, "#n E M \n");
-    for (int n = 0; n <= NB_ITERATIONS; n++) {
-        fprintf(file, "%d %d %d \n", n, tabE[n], tabM[n]);
+    fprintf(file, "#T EMoy MMoy \n");
+    for(int t=0; t < NB_TEMPERATURES; t++){
+        double T = T_MIN + t * (T_MAX - T_MIN) / NB_TEMPERATURES;
+        fprintf(file, "%f %f %f \n", T, tabEMoy[t], tabMMoy[t]);
     }
 
     fclose(file);
