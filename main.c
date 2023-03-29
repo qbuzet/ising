bff0a1fe01db1259a4033710fbfe0a56ac44ef2e
Quentin Buzet <quentin.buzet@telecom-paris.fr>
2023-03-29 11:01:17 +0200

    Ajout de tests, correction randint, correction calcul énergie, utilisation de double...

--- main.c
+++ main.c
@@ -10,22 +10,73 @@
 
 //Constante de la simulation
 #define SEED 0
-#define NB_LIGNES 25
-#define NB_COLONNES 25
-#define SPIN_DEF 0 //0 = GRILLE ALEATOIRE, 1 = GRILLE UP, -1 = GRILLE DOWN
-#define NB_ITERATIONS 1000000
+#define NB_LIGNES 10
+#define NB_COLONNES 10
+#define SPIN_DEF 1 //0 = GRILLE ALEATOIRE, 1 = GRILLE UP, -1 = GRILLE DOWN
+#define NB_ITERATIONS 10000
 
 #define J 1.0
 #define kB 1.0
 
 #define T_MIN 1.0
 #define T_MAX 10.0
-#define NB_TEMPERATURES 100
+#define NB_TEMPERATURES 10
 
 #define FILENAME "./out.txt"
 
 int randint(int a, int b);
 double rand01();
+
+int test_alea(int argc, char* argv[]){
+    if(SEED == 0){
+        srand(time(NULL));
+    } else {
+        srand(SEED);
+    }
+
+    FILE* file = fopen(FILENAME, "w+");
+    if (!file) {
+        printf("Impossible d'ouvrir le fichier");
+    }
+
+    fprintf(file, "#t double int \n");
+    for(int t=0; t < NB_TEMPERATURES; t++){
+        double d = rand01();
+        int i = randint(1,5);
+        fprintf(file, "%d %f %d \n", t, d, i);
+    }
+
+    fclose(file);
+
+    return 0;
+}
+
+int test_bord(int argc, char* argv[]){
+    int d[4][2] = {{0,1},{0,-1},{1,0},{-1,0}};
+
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            printf("%d", -2%3);
+            
+            for(int k = 0; k < 4; k++){
+                int iv = (i + d[k][0]) % 3;
+                int jv = (j + d[k][1]) % 3;
+
+                if(iv < 0) iv += 3;
+                if(jv < 0) jv += 3;
+
+                printf("(%d %d) ", iv,jv);
+            }
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
+
+int test_energie(int argc, char* argv[]){
+    return 0;
+}
 
 int main(int argc, char* argv[]){
     //Initialisation du générateur de nombre aléatoire
@@ -82,6 +133,10 @@ int main(int argc, char* argv[]){
                 for(int k = 0; k < 4; k++){
                     int iv = (i + d[k][0]) % NB_LIGNES;
                     int jv = (j + d[k][1]) % NB_COLONNES;
+
+                    if(iv < 0) iv += NB_LIGNES;
+                    if(jv < 0) jv += NB_COLONNES;
+
                     E -= J * grille[i][j] * grille[iv][jv];
                 }
             }
@@ -102,8 +157,8 @@ int main(int argc, char* argv[]){
         for(int n = 1; n <= NB_ITERATIONS; n++){
 
             //Choix d'un spin à renverser
-            int spin_ligne = randint(0, NB_LIGNES-1); //Exclu ici
-            int spin_colonne = randint(0, NB_COLONNES-1); //Exclu ici
+            int spin_ligne = randint(0, NB_LIGNES);
+            int spin_colonne = randint(0, NB_COLONNES);
             //printf("Spin à renverser : %d %d\n", spin_ligne, spin_colonne);
 
             //Calcul de l'énergie de la nouvelle configuration
@@ -111,13 +166,13 @@ int main(int argc, char* argv[]){
             for(int k = 0; k < 4; k++){
                 int iv = (spin_ligne + d[k][0]) % NB_LIGNES;
                 int jv = (spin_colonne + d[k][1]) % NB_COLONNES;
-                nE -= (double) J * (-2*grille[spin_ligne][spin_colonne]) * grille[iv][jv]; //2* car il faut ajouter également l'énergie déjà présente
+                nE -= (double) -2 * J * grille[spin_ligne][spin_colonne] * grille[iv][jv]; //2* car il faut ajouter également l'énergie déjà présente
             }
 
             //Calcul de l'aimantation de la nouvelle configuration
-            int nM = M - 2*grille[spin_ligne][spin_colonne]; //il faut soustraire en plus l'état d'aimantation actuel de la case 
+            int nM = M - 2 * grille[spin_ligne][spin_colonne]; //il faut soustraire en plus l'état d'aimantation actuel de la case 
 
-            int DE = nE - E;
+            double DE = nE - E;
             if(DE < 0){
                 //Renversement du spin
                 grille[spin_ligne][spin_colonne] = -grille[spin_ligne][spin_colonne];
@@ -137,13 +192,13 @@ int main(int argc, char* argv[]){
 
             //On stocke les nouvelles valeurs de l'énergie et de l'aimantation
             tabE[n] = E;
-            tabM[n] = abs(M);
+            tabM[n] = M;
         }
 
         //Calcul de l'énergie et de l'aimantation moyenne
         for (int n = 0; n < NB_ITERATIONS + 1; n++) {
             EMoy += (double) tabE[n];
-            MMoy += (double )tabM[n];
+            MMoy += (double) tabM[n];
         }
         EMoy /= (double)(NB_ITERATIONS + 1);
         MMoy /= (double)(NB_ITERATIONS + 1);
@@ -159,11 +214,10 @@ int main(int argc, char* argv[]){
 
         fprintf(file, "#n E M \n");
         for (int n = 0; n <= NB_ITERATIONS; n++) {
-            fprintf(file, "%d %d %d \n", n, tabE[n], tabM[n]);
+            fprintf(file, "%d %f %d \n", n, tabE[n], tabM[n]);
         }
 
-        fclose(file);
-        */
+        fclose(file);*/
     }
 
     //Ecriture des résultats dans le fichier de sortie
@@ -183,9 +237,9 @@ int main(int argc, char* argv[]){
     return 0;
 }
 
-//Variable aléatoire uniforme entière entre a et b inclus
+//Variable aléatoire uniforme entière entre a et b exclus
 int randint(int a, int b){
-    return (int) (a + (double) (b - a) * ((double) rand() / RAND_MAX)) + 1;
+    return (int) (a + (double) (b - a) * ((double) rand() / RAND_MAX));
 }
 
 //Variable aléatoire uniforme réelle dans l'intervalle [0,1]
